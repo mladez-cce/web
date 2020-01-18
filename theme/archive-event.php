9d272d1f2213ffd7405f73936b81f34b709b7174
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-01-18 21:44:07 +0100

    feat: update event archive page to support custom pagination
    The first / landing page show the closest upcoming events. The user can
    transparently explore more upcoming events or past events.
    
    The events are sorted by start date.

--- theme/archive-event.php
+++ theme/archive-event.php
@@ -1,4 +1,54 @@
 <?php
+// Template for the "archive page" of the "event" post type (this is the calendar essentially).
 
-// Template for an "archive page" of the "event" post type (this is the calendar essentially).
-view();
+function create_anchor_tag($url, $label, $attr) {
+	return
+		"<a href=\"" . $url . "\" " . $attr . ">" .
+			preg_replace("/&([^#])(?![a-z]{1,8};)/i", "&#038;$1", $label) . "</a>";
+}
+
+/**
+ * Returns the pagination link for previous events. If upcoming events are shown and this is the
+ * first page (= the nearest upcoming events), the link switches to past events.
+ *
+ * @param string $label
+ * @return string|void full HTML anchor tag for the previous page or NULL if there is none
+ */
+function get_previous_posts_link_for_events($label = NULL) {
+	$previous_posts_link = get_previous_posts_link($label);
+
+	if ($previous_posts_link === NULL && !show_past_events()) {
+		$previous_posts_link =
+			create_anchor_tag(
+				add_query_arg(SHOW_PAST_EVENTS_VARNAME, true, get_current_url()),
+				$label,
+				apply_filters("next_posts_link_attributes", ""));
+	}
+
+	return $previous_posts_link;
+}
+
+/**
+ * Returns the pagination link for next events. If past events are shown and this is the first
+ * page (= the latest past events), the link switches to upcoming events.
+ *
+ * @param string $label
+ * @return string|void full HTML anchor tag for the next page or NULL if there is none
+ */
+function get_next_posts_link_for_events($label = NULL) {
+	$next_posts_link = get_next_posts_link($label);
+
+	if ($next_posts_link === NULL && show_past_events()) {
+		$next_posts_link =
+			create_anchor_tag(
+				add_query_arg(SHOW_PAST_EVENTS_VARNAME, false, get_current_url()),
+				$label,
+				apply_filters("next_posts_link_attributes", ""));
+	}
+
+	return $next_posts_link;
+}
+
+view(NULL, [
+	"previous_posts_link" => get_previous_posts_link_for_events("Předešlé události"),
+	"next_posts_link" => get_next_posts_link_for_events("Následující události")]);
