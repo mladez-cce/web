e2d15c98fcafb374bb7dd2f33dd39bb30a90367f
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-01-25 17:51:26 +0100

    feat: print latest posts on the homepage

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,4 +1,12 @@
 <?php
 
+$latestPosts = get_posts([
+	"orderby" => "date",
+	"order" => "DESC",
+	"posts_per_page" => 10,
+]);
+
 // Template for 'Homepage' as configured in 'Settings > Reading'
-view();
+view([
+	"latestPosts" => $latestPosts
+]);
