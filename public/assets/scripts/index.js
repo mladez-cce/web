6e41c4827e2ad4a2ef237d7e62303f7c9eccce1d
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-02-09 17:18:23 +0100

    feat: implement a simple parallax effect on the header

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,48 @@
+/**
+ * Adds a simple parallax effect to an element.
+ *
+ * @param elementSelector a jQuery-like selector to identify the animated element
+ * @param updateFn a function executed on each scroll event that should update the element.
+ *   Receives the object's styles as the first argument and window.pageYOffset as the second
+ *   argument.
+ */
+function addParallaxEffect(elementSelector, updateFn) {
+	window.addEventListener("load", function () {
+		const element = document.querySelector(elementSelector);
+
+		if (!element) {
+			return;
+		}
+
+		window.addEventListener("scroll", function() {
+			updateFn(element.style, window.pageYOffset);
+		});
+
+		updateFn(element.style, window.pageYOffset);
+	});
+}
+
+// Add a parallax effect to the blue and yellow header backgrounds (which are both set on the same
+// element).
+addParallaxEffect(
+	".page-header",
+	function (style, offset) {
+		const BLUE_BG_TOP_OFFSET = 40; // Needs to be kept in sync with the styles.
+		const SCROLL_SPEED = 1/10;
+
+		const blueBgPosition = "center " + (BLUE_BG_TOP_OFFSET + offset * SCROLL_SPEED) + "px";
+		const yellowBgPosition = "center " + (-1 * offset * SCROLL_SPEED) + "px";
+
+		style.backgroundPosition = blueBgPosition + ", " + yellowBgPosition;
+	});
+
+// Add a parallax effect to the header container with the boat image.
+addParallaxEffect(
+	".page-header__container",
+	function (style, offset) {
+		const SCROLL_SPEED = 1/4;
+		const FADE_SPEED = 1/400;
+
+		style.transform = "translateY(" + offset * SCROLL_SPEED + "px)";
+		style.opacity = Math.max(1 - offset * FADE_SPEED, 0);
+});
