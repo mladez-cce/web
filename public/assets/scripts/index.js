f9631149ba3e1362ee4cf435f5df1e5e187786b1
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-04-04 11:36:18 +0200

    fix: update the parallax effects to work with the new header structure.
    Minor code clean-ups have been carried out in the parallax JS
    implementation.

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,10 +1,11 @@
 /**
  * Adds a simple parallax effect to an element.
  *
- * @param elementSelector a jQuery-like selector to identify the animated element
- * @param updateFn a function executed on each scroll event that should update the element.
- *   Receives the object's styles as the first argument and window.pageYOffset as the second
- *   argument.
+ * @param elementSelector a jQuery-like selector to identify the animated
+ * 		element
+ * @param updateFn a function executed on each scroll event that should update
+ * 		the element. Receives the object's styles as the first argument and
+ * 		window.pageYOffset as the second argument.
  */
 function addParallaxEffect(elementSelector, updateFn) {
 	window.addEventListener("load", function () {
@@ -22,26 +23,38 @@ function addParallaxEffect(elementSelector, updateFn) {
 	});
 }
 
-// Add a parallax effect to the blue and yellow header backgrounds (which are both set on the same
-// element).
+// Add a parallax effect to the blue and yellow header backgrounds (which are
+// both set on the same element).
 addParallaxEffect(
-	".page-header",
+	".page-jumbotron-header",
 	function (style, offset) {
 		const BLUE_BG_TOP_OFFSET = 40; // Needs to be kept in sync with the styles.
-		const SCROLL_SPEED = 1/10;
+		const SCROLL_SPEED = 0.1;
 
-		const blueBgPosition = "center " + (BLUE_BG_TOP_OFFSET + offset * SCROLL_SPEED) + "px";
-		const yellowBgPosition = "center " + (-1 * offset * SCROLL_SPEED) + "px";
+		const blueBgPosition =
+			"center " + (BLUE_BG_TOP_OFFSET + offset * SCROLL_SPEED) + "px";
+		const yellowBgPosition = "center " + -1 * offset * SCROLL_SPEED + "px";
 
 		style.backgroundPosition = blueBgPosition + ", " + yellowBgPosition;
 	});
 
-// Add a parallax effect to the header container with the boat image.
+// Add a parallax effect to the header text.
 addParallaxEffect(
-	".page-header__container",
+	".page-jumbotron-header h1",
 	function (style, offset) {
-		const SCROLL_SPEED = 1/4;
-		const FADE_SPEED = 1/400;
+		const SCROLL_SPEED = 0.011;
+		const FADE_SPEED = 0.003;
+
+		style.transform = "translateY(" + offset * SCROLL_SPEED + "px)";
+		style.opacity = Math.max(1 - offset * FADE_SPEED, 0);
+	});
+
+// Add a parallax effect to the header boat image.
+addParallaxEffect(
+	".page-jumbotron-header__boat",
+	function (style, offset) {
+		const SCROLL_SPEED = 0.12;
+		const FADE_SPEED = 0.002;
 
 		style.transform = "translateY(" + offset * SCROLL_SPEED + "px)";
 		style.opacity = Math.max(1 - offset * FADE_SPEED, 0);
