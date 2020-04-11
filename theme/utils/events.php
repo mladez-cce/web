c9ad15be24d5863917e9e1a3de4194f4ef058b0b
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-04-11 11:25:41 +0200

    feat: mark past events with an explicit CSS class

--- theme/utils/events.php
+++ theme/utils/events.php
@@ -23,3 +23,17 @@ function wml_get_event_date($Post) {
 		($sameMonth ? $startDate->format("j.") : $startDate->format("j. n."))
 		. " až " . $endDate->format("j. n. Y");
 }
+
+/**
+ * Returns if an event is over (it's end date is in the past).
+ *
+ * @param WP_Post $Post a WordPress post of the "event" type.
+ * @return bool true if the event is over or if $Post is not a valid event.
+ */
+function wml_is_event_over($Post) {
+	if (empty($Post->end_date)) {
+		return true;
+	}
+
+	return date_create_from_format("Y-m-d", $Post->end_date) < date_create("now");
+}
