7a8c999a8364051d830fd5a940b926f60cc0f390
Tobiáš Potoček <tobiaspotocek@gmail.com>
2020-04-11 11:10:04 +0200

    fix: broken event pagination
    How come initial testing did not reveal this?

--- theme/archive-event.php
+++ theme/archive-event.php
@@ -10,18 +10,21 @@
  * @param string $label
  * @return string|void full HTML anchor tag for the previous page or NULL if there is none
  */
-function get_previous_posts_link_for_events($label = NULL) {
-	$previous_posts_link = get_previous_posts_link($label);
+function get_previous_events_link($label = NULL) {
+	// Past events are in reversed order.
+	$previous_events_link = show_past_events()
+		? get_next_posts_link($label)
+		: get_previous_posts_link($label);
 
-	if ($previous_posts_link === NULL && !show_past_events()) {
-		$previous_posts_link =
+	if ($previous_events_link === NULL && !show_past_events()) {
+		$previous_events_link =
 			wml_create_anchor_tag(
 				add_query_arg(SHOW_PAST_EVENTS_VARNAME, true, wml_get_current_url()),
 				$label,
 				apply_filters("next_posts_link_attributes", ""));
 	}
 
-	return $previous_posts_link;
+	return $previous_events_link;
 }
 
 /**
@@ -31,20 +34,23 @@ function get_previous_posts_link_for_events($label = NULL) {
  * @param string $label
  * @return string|void full HTML anchor tag for the next page or NULL if there is none
  */
-function get_next_posts_link_for_events($label = NULL) {
-	$next_posts_link = get_next_posts_link($label);
+function get_next_events_link($label = NULL) {
+	// Past events are in reversed order.
+	$next_events_link = show_past_events()
+		? get_previous_posts_link($label)
+		: get_next_posts_link($label);
 
-	if ($next_posts_link === NULL && show_past_events()) {
-		$next_posts_link =
+	if ($next_events_link === NULL && show_past_events()) {
+		$next_events_link =
 			wml_create_anchor_tag(
 				add_query_arg(SHOW_PAST_EVENTS_VARNAME, false, wml_get_current_url()),
 				$label,
 				apply_filters("next_posts_link_attributes", ""));
 	}
 
-	return $next_posts_link;
+	return $next_events_link;
 }
 
 view(NULL, [
-	"previous_posts_link" => get_previous_posts_link_for_events("Předešlé události"),
-	"next_posts_link" => get_next_posts_link_for_events("Následující události")]);
+	"previous_events_link" => get_previous_events_link("Předešlé události"),
+	"next_events_link" => get_next_events_link("Následující události")]);
